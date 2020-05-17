34f55bdfc68ab799a9daf6705892e3b33e3151e3
mgt <1152719547@qq.com>
2020-05-17 16:45:20 +0800

    【新增】常见问题解答详情页、专业列表

---
+++ miniprogram/pages/dreamFactory/majorlist/majorlist.js
@@ -0,0 +1,112 @@
+// miniprogram/pages/dreamFactory/majorlist/majorlist.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    majorlist:[{
+      classAlias:'经济学',
+      majors:[
+        {
+          majorId:101,
+          name:'金融工程',
+        },{
+          majorId:102,
+          name:'经济学',
+        },{
+          majorId:103,
+          name:'金融学',
+        },{
+          majorId:104,
+          name:'国际经济与贸易',
+        },
+        {
+          majorId:104,
+          name:'国际经济与贸易',
+        },
+      ],
+      },
+      {
+        classAlias:'经济学',
+        majors:[
+          {
+            majorId:101,
+            name:'金融工程',
+          },{
+            majorId:102,
+            name:'经济学',
+          },{
+            majorId:103,
+            name:'金融学',
+          },{
+            majorId:104,
+            name:'国际经济与贸易',
+          },
+          {
+            majorId:104,
+            name:'国际经济与贸易',
+          },
+        ],
+        },
+    ]
+  },
+  jumpDetail:function(e){
+    console.log(e)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
