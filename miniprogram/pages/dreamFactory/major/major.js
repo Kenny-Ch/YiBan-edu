0071007cee38f9dd1e56576b3a57470e39585019
y894577 <44811269+y894577@users.noreply.github.com>
2020-06-03 23:49:17 +0800

    【新增】majorlist列表渲染

--- miniprogram/pages/dreamFactory/major/major.js
+++ miniprogram/pages/dreamFactory/major/major.js
@@ -44,7 +44,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options.majorId)
+    const db = wx.cloud.database()
+    db.collection('').where({
+      flag:'major',
+      
+    })
   },
 
   /**
