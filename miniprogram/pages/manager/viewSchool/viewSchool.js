1eac6536da49b54ba1c5d5ddae47a7f154de0470
mgt <1152719547@qq.com>
2020-07-18 21:12:18 +0800

    【新增】网校查看页面

---
+++ miniprogram/pages/manager/viewSchool/viewSchool.js
@@ -0,0 +1,85 @@
+// miniprogram/pages/manager/viewSchool/viewSchool.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   school: [{
+          id: 1,
+          name: '广大教院网校',
+          schoolID: '0001',
+          Num: [55, 78],
+        },
+        {
+          id: 2,
+          name: '广大外文网校',
+          schoolID: '0002',
+          Num: [55, 78],
+        },
+        {
+          id: 3,
+          name: '企通网校',
+          schoolID: '0003',
+          Num: [57, 78],
+        }
+      ],
+    bottomtext:'------到底啦------',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
