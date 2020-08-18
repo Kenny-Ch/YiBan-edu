c30a8c32088937eb84e8952a8ac174f8aac6ce58
y894577 <44811269+y894577@users.noreply.github.com>
2020-08-19 00:22:50 +0800

    【修复】发起人冲突问题

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -172,13 +172,13 @@ Page({
         var isReview = app.globalData.userInfo.isCheck
         //匹配失败
         var isMatchFail = app.globalData.userInfo.matchReject
-        //老师未完成第二次简历填写
+        //老师是否完成第二次简历填写
         var isTeacherFinish = (app.globalData.userInfo.otherInfo != undefined) ? (true) : (false)
 
 
 
         //寻找你的以伴老师
-        if (isNew || isAmbassador || isStudent) {
+        if (!isOther && (isNew || isAmbassador || isStudent)) {
           //新用户、爱心大使和学生显示
           let item = {};
           item.id = 0;
@@ -214,7 +214,7 @@ Page({
           item.title = "一起迈向公益之路";
           item.small_title = "只要你有足够的热情，想为公益事业做出一份自己的贡献，都可以申请成为以伴志愿者！";
           item.button = "加入我们";
-          if (!isTeacherFinish && isVolunteer) {
+          if ((isOther || isVolunteer) && !isTeacherFinish) {
             //老师未完成第二次简历填写
             item.url = '../join/workingAbility/workingAbility'
           } else if (isReview == 0) {
@@ -255,8 +255,8 @@ Page({
           item.small_title = "以伴有着成百上千的网校，您可以借助以伴公益教育平台，实现您的公益梦想！";
           if (isSponsor) {
             //已经绑定发起人
-            item3.button = "查看我的网校";
-            item3.url = "../sponsor/managerSponsor/managerSponsor?schoolID=" + app.globalData.userInfo.schoolID;
+            item.button = "查看我的网校";
+            item.url = "../sponsor/managerSponsor/managerSponsor?schoolID=" + app.globalData.userInfo.schoolID;
           } else {
             //没有绑定发起人
             item.button = "建立我的网校";
