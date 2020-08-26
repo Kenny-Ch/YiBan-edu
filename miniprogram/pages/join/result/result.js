a811acd80ca6877b03e844570a023530d32efecd
y894577 <44811269+y894577@users.noreply.github.com>
2020-08-26 14:14:50 +0800

    【新增】二维码更新

--- miniprogram/pages/join/result/result.js
+++ miniprogram/pages/join/result/result.js
@@ -5,11 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    result:true,
-    QR:'cloud://yiban-edu.7969-yiban-edu-1301806073/community/growUp.png',
-    shuoming:'请尽快加入以伴志愿者面试群聊，以便快速通过审核。',
+    result: true,
+    QR: 'cloud://yiban-edu.7969-yiban-edu-1301806073/community/growUp.png',
+    shuoming: '请尽快加入以伴志愿者面试群聊，以便快速通过审核。',
   },
-  previewImage: function(e) {
+  previewImage: function (e) {
     wx.previewImage({
       urls: e.target.dataset.url.split(',')
       // 需要预览的图片http链接  使用split把字符串转数组。不然会报错
@@ -19,7 +19,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const app = getApp()
+    let that = this
+    wx.cloud.downloadFile({
+      fileID: 'cloud://yiban-edu.7969-yiban-edu-1301806073/networkSchoolQR/' + app.globalData.userInfo.schoolID + '.jpg', // 文件 ID
+      success: res => {
+        // 返回临时文件路径
+        console.log(res)
+        that.setData({
+          QR: res.tempFilePath
+        })
+      },
+      fail: console.error
+    })
   },
 
   /**
